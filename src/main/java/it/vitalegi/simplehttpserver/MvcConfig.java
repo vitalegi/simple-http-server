003e5ceab160bb75ef48caa3f381d9b2b14579a7
Giorgio Vitale <giorgio.vitale.90@gmail.com>
2021-01-22 19:28:17 +0100

    handles index.html pages

--- src/main/java/it/vitalegi/simplehttpserver/MvcConfig.java
+++ src/main/java/it/vitalegi/simplehttpserver/MvcConfig.java
@@ -1,11 +1,16 @@
 package it.vitalegi.simplehttpserver;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.PathResourceResolver;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,6 +28,21 @@ public class MvcConfig implements WebMvcConfigurer {
 		log.info("Static resources: {}", staticResources);
 		registry.addResourceHandler("/**")//
 				.addResourceLocations(staticResources)//
-				.setCachePeriod(10);
+				.setCachePeriod(10).resourceChain(true).addResolver(new PathResourceResolver() {
+
+					@Override
+					protected Resource getResource(String resourcePath, Resource location) throws IOException {
+						Resource relative = location.createRelative(resourcePath);
+						if (relative.getFile().isDirectory()) {
+							return super.getResource(resourcePath + "/index.html", location);
+						}
+						return super.getResource(resourcePath, location);
+					}
+				});
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addRedirectViewController("/", "index.html");
 	}
 }
